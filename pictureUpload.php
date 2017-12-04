bd7f3175b623cadba97ff1312c28b1ff076a4b6b
Yash Upadhyay <yash.upadhyay97@gmail.com>
2017-12-04 12:58:35 -0500

    got something inserting, not correct image tho

--- pictureUpload.php
+++ pictureUpload.php
@@ -3,15 +3,16 @@
     require_once("dbLogin.php");
     require_once("setupDB.php");
     session_start();
-
     $connection = initDBConnection($host, $user, $dbpassword, $database);
+
+    $temp = $_FILES["fileToUpload"]["name"];
     $uid = $_SESSION['uid'];
     $filename = $uid . '.jpg';
-    $docMimeType = "image/jpg";
-    $fileToInsert = $_FILES['filename']['name'];
+    $docMimeType = "image/jpeg";
 
+    $fileToInsert = $_FILES['fileToUpload']['tmp_name'];
     $fileData = addslashes(file_get_contents($fileToInsert));
-    $query = "insert into tbltas (uid, pictureName, docMimeType, picture) values ('$uid', 'test', '$docMimeType', '$fileData')";
+    $query = "REPLACE INTO tbltas (uid, pictureName, docMimeType, picture) VALUES ($uid, '$filename', '$docMimeType', '$temp')";
     $connection->query($query);
     header("Location: classList.php");
 
