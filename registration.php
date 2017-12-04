9c452037d113aa18e1185a858dd62d7e4af7ed20
Kyle Foster <kylefoster596@gmail.com>
2017-12-04 01:58:13 -0500

    functionality for registration.php

--- registration.php
+++ registration.php
@@ -4,7 +4,18 @@ require_once("dbLogin.php");
 require_once("setupDB.php");
 
 $title = "Registration";
+if(isset($_POST["submitRegistration"])){
+    $connection = initDBConnection($host, $user, $dbpassword, $database);
+    $firstname = $_POST["firstname"];
+    $lastname = $_POST["lastname"];
+    $uid = $_POST["uid"];
+    $email = $_POST["email"];
+    $pass = $_POST["password"];
+    $query = "insert into tblusers values('$firstname', '$lastname', '$uid', '$email', '$pass')";
+    $connection->query($query);
+}
 $topPart = <<<EOBODY
+<script src="validate.js"></script>
 <style>
 .form-group {
     width: 25%;
@@ -19,11 +30,18 @@ $topPart = <<<EOBODY
 			<strong>First Name: </strong><input type="text" class="form-control" name="firstname" id="firstname" required/><br/><br/>
 			<strong>Last Name: </strong><input type="text" class="form-control" name="lastname" id="lastname" required/><br/><br/>
 			<strong>UID: </strong><input type="text" class="form-control" name="uid" id="uid" required/><br/><br/>
+			<strong>Email: </strong><input type="email" class="form-control" name="email" id="email" required><br /><br />
             <strong>Password: </strong><input type="password" class="form-control" name="password" required/><br/><br/>
 
 			<input type="submit" class="btn btn-info" name="submitRegistration" value="Register" style="display: table; margin: 0 auto;"/>
 		</form>
+		<br />
+		<form action="login.php" method="post">
+		    <input type="submit" class="btn btn-info" id="returnToLogin" name="returnToLogin"  value="Return to Login Page" style="...">
+        </form>
 </div>
+        
+
 EOBODY;
 
 
