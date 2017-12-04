0b6e4796d054f0da981210f5ef31dbecba72d3d7
ninadinh31 <ninadinh31@gmail.com>
2017-12-04 04:57:03 -0500

    help next working

--- support.js
+++ support.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function confirmDelete(courseid) {
     var c = confirm("Are you sure you want to delete this entry?");
     if (c) {
@@ -10,4 +12,10 @@ function confirmAdd(courseid) {
     if (c) {
         window.location.href = "deleteCourse.php?courseid=" + courseid;
     }
+}
+
+function displayCurrStud(courseid) {
+    console.log("here");
+    let currStudId = "currStud" + courseid;
+    document.getElementById(currStudId).innerHTML("hello");
 }
