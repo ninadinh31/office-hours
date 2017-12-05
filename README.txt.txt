d83bc864d979da93d289b5723046f77f5d5f8ae2
ninadinh31 <ninadinh31@gmail.com>
2017-12-05 18:38:09 -0500

    lets finish this mofo

---
+++ README.txt.txt
@@ -0,0 +1,8 @@
+Ngoc-Han Dinh | 113539381
+Kyle Foster | 113568084
+Yash Upadhyay | 114120120
+
+
+start with login.php, use tester info:
+uid: 1234
+pw: 1234
--- pictureUpload.php
+++ pictureUpload.php
@@ -3,17 +3,14 @@
     require_once("dbLogin.php");
     require_once("setupDB.php");
     session_start();
-    $connection = initDBConnection($host, $user, $dbpassword, $database);
 
-    $temp = $_FILES["fileToUpload"]["name"];
+    $connection = initDBConnection($host, $user, $dbpassword, $database);
     $uid = $_SESSION['uid'];
     $filename = $uid . '.jpg';
+    $image = addslashes(file_get_contents($_FILES['fileToUpload']['tmp_name']));
+    $image_name = addslashes($_FILES['fileToUpload']['name']);
     $docMimeType = "image/jpeg";
-
-    echo $_SESSION['uid'];
-    $fileToInsert = $_FILES['fileToUpload']['tmp_name'];
-    $fileData = addslashes(file_get_contents($fileToInsert));
-    $query = "REPLACE INTO tbltas (uid, pictureName, docMimeType, picture) VALUES ($uid, '$filename', '$docMimeType', '$temp')";
+    $query = "REPLACE INTO tbltas (uid, pictureName, docMimeType, picture) VALUES ('$uid', '$filename', '$docMimeType', '$image')";
     $connection->query($query);
     header("Location: classList.php");
 
